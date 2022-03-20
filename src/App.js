46f7614b2c59348afd36d0691f44e580937273ff
hesham <ehesham26@yahoo.com>
2022-03-20 14:06:38 +0200

    Sample Commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import Home from './Home';
+import MenuCart from './component/MenuCart/MenuCart';
+import NotFound from './NotFound';
+
+import './App.css';
+
+
+class App extends Component {
+
+  state = {
+    imgState: '', 
+    name: '', 
+    quantity: 0, 
+    price: 0, 
+  }
+  handlerState = (imgUrl, name, quantity, price) => {
+    this.setState({imgState: imgUrl, name: name, quantity: quantity, price: price})
+  }
+  handlerImageState = (imgUrl) => {
+    this.setState({...this.state, imgState: imgUrl})
+  }
+    render() {
+        return (
+          <Router>
+          <div className="App">
+          
+          <Routes>
+              <Route exact={false} path="/" element={<Home handelState={this.handlerState} handelImage={this.handlerImageState} />} />
+              <Route path="/MenuCart" element={<div><Home handelState={this.handlerState} handelImage={this.handlerImageState} /><MenuCart statePro={this.state} /> </div> } />
+              <Route path="*" element={<NotFound />} />
+              </Routes>
+
+          </div>
+          </Router>
+        )
+    } 
+
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import './App.css';
+
+import Header from './component/Header/Header';
+import MainNav from './component/Header/MainNav';
+import Loadmenu from './component/Header/Loadmenu';
+import Description from './component/Description/Description';
+import Productimg from './component/Description/Productimg';
+import SimilarProduct from './component/SimilarProduct/SimilarProduct';
+import Footer from './component/Footer/Footer';
+import NavTop from './component/Header/NavTop';
+
+
+import Container from './component/Container';
+
+class Home extends Component {
+    state = {
+        counter: 0
+    }
+    handlerCounter = (num) => {
+        this.setState({counter: num})
+    }
+
+    render() {
+        return (
+            <div className="home">
+            <NavTop />
+            <MainNav counter={this.state.counter} />
+            <Header />
+            <Loadmenu />
+            <Container>
+            <div className="col-lg-6">
+              <Productimg handelImage={this.props.handelImage} />
+            </div>
+            <div className="col-lg-6">
+              <Description handlerCounter={this.handlerCounter} handelState={this.props.handelState} />
+            </div>
+            <SimilarProduct />
+            </Container>
+            <Footer />
+            </div>
+
+        )
+    }
+}
+export default Home
